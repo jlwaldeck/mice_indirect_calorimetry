fc9673650088655bc7542ad1120580cb8034af81
Jarrod Waldeck <jarrod.waldeck@outlook.com>
2025-04-13 12:21:29 -0500

    Fix bugs with column mapping
    Previous code was over-writing original column vs. adding a new one.

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def load_data(file_path):
     """Load data from a CSV file."""
     try:
@@ -10,36 +12,40 @@ def load_data(file_path):
         return None
 
 
-def apply_mapping(df, column_name, column_config):
-    """Apply mapping and default value to a specific column.
+def apply_mapping(df, column_config):
+    """Apply mapping and default value to create a new column based on an existing column.
     Args:
         df (pd.DataFrame): The DataFrame to transform.
-        column_name (str): The name of the column to transform.
-        column_config (dict): Configuration for the column including mapping and default value.
+        column_config (dict): Configuration for the column including source, target, mapping, and default value.
     Returns:
         pd.DataFrame: The transformed DataFrame.
     Notes:
-        This function also allows for changing the data type of the column.
-        If config contains explict dtype, it will convert the column to that type.
-        Otherwise, it will keep the original data type inferred by pandas when loading csv.
+        This function creates a new column based on the mapping of an existing column.
     """
 
+    source_column = column_config.get('source')
+    target_column = column_config.get('target')
     mapping = column_config.get('mapping', {})
+    print(mapping)
     default_value = column_config.get('default', None)
     dtype = column_config.get('dtype', None)  # Get the desired data type
 
-    # Apply the specified data type if provided
+    # Apply the specified data type to the new column if provided
     if dtype:
-        df[column_name] = df[column_name].astype(dtype)
+        print(dtype)
+        df[source_column] = df[source_column].astype(dtype)
 
-    # Apply the mapping with the default value
-    df[column_name] = df[column_name].map(mapping).fillna(default_value)
+    if not source_column or not target_column:
+        raise ValueError("Both 'source' and 'target' must be specified in the column configuration.")
+
+    # Create the new column based on the mapping with the default value
+    df[target_column] = df[source_column].map(mapping).fillna(default_value)
 
     return df
+
 
 def transform_via_map(df, column_mappings):
     """Transform columns based on mappings in the config."""
     for column_name, column_config in column_mappings.items():
-        if column_name in df.columns:
-            df = apply_mapping(df, column_name, column_config)
+        df = apply_mapping(df, column_config)
     return df
--- src/lab_analysis.py
+++ src/lab_analysis.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import yaml
-from data_processing import transform_via_map
+from data_processing import load_data, transform_via_map
 
 # TODO: Move this path to a config file or environment variable
 raw_data_path = ('C:/Users/nwald/OneDrive/Desktop/202305_Sable_DATBFX_Females_n16_AL_RC/202305_DATBFX_Females_RC_AL_Sable_n16.csv')
@@ -10,22 +10,11 @@ raw_data_path = ('C:/Users/nwald/OneDrive/Desktop/202305_Sable_DATBFX_Females_n1
 script_dir = os.path.dirname(__file__)
 config_file_path = os.path.join(script_dir, 'config.yaml')
 
-# Load the YAML configuration
+# Load the columm mapping configuration from YAML
 with open(config_file_path, 'r') as file:
     config = yaml.safe_load(file)
-
 column_mappings = config['column_mappings']
 
-def load_data(file_path):
-    """Load data from a CSV file."""
-    try:
-        data = pd.read_csv(file_path)
-        print(f"Data loaded successfully from {file_path}")
-        print(f"Data shape: {data.shape}")
-        return data
-    except Exception as e:
-        print(f"Error loading data: {e}")
-        return None
 
 def main():
 
@@ -34,7 +23,11 @@ def main():
     
     # TODO: Replace with logger statements and/or assertions as needed
     if df is not None:
-        print(df.head())
+        print(df.tail())
+        print(df['four_hours'].unique())
+        print(df['two_hours'].unique())
+        print(df['one_hour'].unique())
+        print(df['day_night'].unique())
     else:
         print("No data to display after transformation.")
 
